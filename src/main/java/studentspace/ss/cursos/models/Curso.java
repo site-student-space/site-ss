479bde01c9c9efb556fad5dae08004151030d1c8
Vinicius Gabriel <vini.gabriel.m@gmail.com>
2023-05-03 14:25:55 -0300

    Criando modelo para Curso

---
+++ src/main/java/studentspace/ss/cursos/models/Curso.java
@@ -0,0 +1,42 @@
+package studentspace.ss.cursos.models;
+
+public class Curso {
+
+	private String nome;
+	private String descricao;
+	private String data;
+	private String valor;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+}
