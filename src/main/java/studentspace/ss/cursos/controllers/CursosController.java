76193951a7e196da394e3418535415a20099655c
Vinicius Gabriel <vini.gabriel.m@gmail.com>
2023-05-03 15:01:52 -0300

    Criando a página de formulário para cadastro do curso

--- src/main/java/studentspace/ss/cursos/controllers/CursosController.java
+++ src/main/java/studentspace/ss/cursos/controllers/CursosController.java
@@ -3,10 +3,11 @@ package studentspace.ss.cursos.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class CursosController {
 
-	RequestMapping("/cursos/form")
+	@RequestMapping("/cursos/form")
 	public String form() {
 		return "formCurso";
 	}
