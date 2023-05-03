0d751cd8d80f375f39eccd529a8d9ded2d8c871b
Vinicius Gabriel <vini.gabriel.m@gmail.com>
2023-05-03 17:51:21 -0300

    Configurando dependências e fazendo configurações iniciais do banco de dados

--- src/main/java/studentspace/ss/cursos/models/Curso.java
+++ src/main/java/studentspace/ss/cursos/models/Curso.java
@@ -1,11 +1,28 @@
 package studentspace.ss.cursos.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Curso {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nome;
 	private String descricao;
 	private String data;
 	private String valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
