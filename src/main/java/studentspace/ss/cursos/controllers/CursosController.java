ebf4c22c955dc270cd4d8e021c0f29f894d0a640
Vinicius Gabriel <vini.gabriel.m@gmail.com>
2023-05-03 14:36:35 -0300

    Criando o controller de cursos

---
+++ src/main/java/studentspace/ss/cursos/controllers/CursosController.java
@@ -0,0 +1,14 @@
+package studentspace.ss.cursos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CursosController {
+
+	RequestMapping("/cursos/form")
+	public String form() {
+		return "formCurso";
+	}
+	
+}
